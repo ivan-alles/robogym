5bb9f3d270ee5f2e07f01dbf8882639310c9bbf2
Ivan Alles <ivan.alles@gmail.com>
2020-11-24 08:59:05 +0100

    Fix flake8 error

--- setup.py
+++ setup.py
@@ -27,9 +27,9 @@ setuptools.setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    install_requires = [
+    install_requires=[
         'numpy',
         'scipy'
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.6'
 )
